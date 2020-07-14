d8194afed5e421d470b4e1daf944cbe7be9e11a3
Shin Chae Yeon <rachaelshin@naver.com>
2020-07-14 11:57:27 +0900

    [채연] ADD - Spring Boot 프로젝트 생성

---
+++ server/src/main/java/com/kbds/dagapa/DagapaApplication.java
@@ -0,0 +1,13 @@
+package com.kbds.dagapa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DagapaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DagapaApplication.class, args);
+    }
+
+}
---
+++ server/src/test/java/com/kbds/dagapa/DagapaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kbds.dagapa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DagapaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
