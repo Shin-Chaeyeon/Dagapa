316659be2e65481547f364ddb2955954af4f6c10
Shin Chae Yeon <rachaelshin@naver.com>
2020-07-14 17:50:43 +0900

    [채연] ADD - user, contract VO틀, lombok, mybatis

---
+++ server/src/main/java/com/kbds/vo/Contract.java
@@ -0,0 +1,29 @@
+package com.kbds.vo;
+
+import lombok.*;
+
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Builder
+public class Contract {
+    private int contractno;
+    private String lender;
+    private String borrower;
+    private int type;
+    private String goods;
+    private String duedate;
+    private String picture;
+    private String record;
+    private String description;
+    private int agreed;
+
+    public void setDueDate(Timestamp timestamp){
+        this.duedate = new SimpleDateFormat("yyyy-MM-dd").format(timestamp);
+    }
+}
