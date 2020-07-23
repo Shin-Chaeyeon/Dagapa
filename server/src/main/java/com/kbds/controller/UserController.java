1ef77bd5a324da4d4e02cdddc10f651e9b2ce13a
Shin Chae Yeon <rachaelshin@naver.com>
2020-07-23 11:33:04 +0900

    [채연] ADD - user controller도 한글 처리

--- server/src/main/java/com/kbds/controller/UserController.java
+++ server/src/main/java/com/kbds/controller/UserController.java
@@ -9,6 +9,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.UnsupportedEncodingException;
+
 @Api(tags = {"***User***"})
 @RequiredArgsConstructor
 @RestController
@@ -18,7 +22,9 @@ public class UserController {
 
     @ApiOperation(value = "로그인", notes = "")
     @PostMapping("/sign_in")
-    public User login(@RequestBody User user){
+    public User login(@RequestBody User user, HttpServletRequest request, HttpServletResponse response) throws UnsupportedEncodingException {
+        request.setCharacterEncoding("UTF-8");
+        response.setCharacterEncoding("UTF-8");
         User search = userService.findUser(user);
         if(user.getPw().equals(search.getPw())) {
             log.info("[CONTROLLER] :: login()");
