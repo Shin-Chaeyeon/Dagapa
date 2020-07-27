df7b610e48c13a2f79f67a41cdfae6c300977cc4
Shin Chae Yeon <rachaelshin@naver.com>
2020-07-27 14:35:33 +0900

    [채연] DELETE - audiourl, agreed 컬럼 및 연계된 코드 삭제

--- server/src/main/java/com/kbds/vo/Contract.java
+++ server/src/main/java/com/kbds/vo/Contract.java
@@ -22,8 +22,6 @@ public class Contract {
     private String startdate;
     private String duedate;
     private String imageurl;
-    private String audiourl;
     private String description;
-    private int agreed;
     private int status;
 }
