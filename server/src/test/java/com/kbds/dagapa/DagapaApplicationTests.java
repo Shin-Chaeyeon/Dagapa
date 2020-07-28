fe4ac901d53bdd3fbf50e4de65a7357f089c201c
Shin Chae Yeon <rachaelshin@naver.com>
2020-07-28 10:15:21 +0900

    [채연] UPDATE - MERGE 완료

--- server/src/test/java/com/kbds/dagapa/DagapaApplicationTests.java
+++ server/src/test/java/com/kbds/dagapa/DagapaApplicationTests.java
@@ -12,23 +12,7 @@ import org.springframework.context.annotation.ComponentScan;
 class DagapaApplicationTests {
     @Autowired
     private UserDAO userDAO;
-    //@Autowired
-    //private ContractDAO contractDAO;
-
     @Test
     void contextLoads() {
-        userTest();
-        //contractTest();
-    }
-
-    private void userTest() {
-        User user = User.builder()
-                .id("user11")
-                .pw("1234")
-                .nickname("마이트렙카드")
-                .phone("010-1234-5678").build();
-        int userPK = user.getUserno();
-        User result = userDAO.findUser(User.builder().userno(userPK).build());
-        System.out.println(result);
     }
 }
