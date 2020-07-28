e4a987c696be50652be3e7ba436d2a52b2b19489
Jihwan Moon <jihwanla17@naver.com>
2020-07-28 09:30:50 +0900

    [지환] DELETE - 구버전 SERVER 폴더 삭제

--- server/src/main/java/com/kbds/dao/UserDAO.java
+++
@@ -1,7 +0,0 @@
-package com.kbds.dao;
-
-import com.kbds.vo.User;
-
-public interface UserDAO {
-    public User findUser(User user);
-}
--- server/src/main/java/com/kbds/service/FileService.java
+++
@@ -1,7 +0,0 @@
-package com.kbds.service;
-
-import java.io.IOException;
-
-public interface FileService<T> {
-    public void fileSave(T vo) throws IOException;
-}
