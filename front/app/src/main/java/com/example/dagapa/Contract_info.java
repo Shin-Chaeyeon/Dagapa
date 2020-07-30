2dd6a51ce4c03c802218e2722d4e8b6763014a84
Jihwan Moon <jihwanla17@naver.com>
2020-07-30 10:42:38 +0900

    [지환] UPDATE - 계약서 상세정보 이미지 수정

--- front/app/src/main/java/com/example/dagapa/Contract_info.java
+++ front/app/src/main/java/com/example/dagapa/Contract_info.java
@@ -5,22 +5,72 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class Contract_info extends AppCompatActivity {
 
-    TextView info;
+    TextView lender;
+    TextView borrower;
+    TextView startDate;
+    TextView dueDate;
+    TextView goods;
+    ImageView image;
+    ImageView imageMoney;
+    TextView imageInfo;
+
+    TextView goodsment;
+    TextView moneyment;
+
+    int type;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contract_info);
 
-//        info = findViewById(R.id.info);
-//
-//        Intent intent = getIntent();
-//        Contract contract = (Contract) intent.getSerializableExtra("contractInfo");
-//        info.setText(contract.toString());
+        lender = findViewById(R.id.lender_edit);
+        borrower = findViewById(R.id.borrower_edit);
+        startDate = findViewById(R.id.startdate_edit);
+        dueDate = findViewById(R.id.duedate_edit);
+        goods = findViewById(R.id.goods_edit);
+        image = findViewById(R.id.imageView);
+        imageInfo = findViewById(R.id.imageInfo);
+        imageMoney = findViewById(R.id.money_img);
+
+        goodsment = findViewById(R.id.goods_ment);
+        moneyment = findViewById(R.id.money_ment);
+
+        Intent intent = getIntent();
+        Contract contract = (Contract) intent.getSerializableExtra("contractInfo");
+
+        type = contract.getType();
+
+
+        lender.setText(contract.getLender());
+        borrower.setText(contract.getBorrower());
+        startDate.setText(contract.getStartdate());
+        dueDate.setText(contract.getDuedate());
+        goods.setText(contract.getGoods());
+
+        if(type==1){
+            // 물품
+            image.setVisibility(View.VISIBLE);
+            imageInfo.setVisibility(View.VISIBLE);
+            goodsment.setVisibility(View.VISIBLE);
+            moneyment.setVisibility(View.GONE);
+            imageMoney.setVisibility(View.GONE);
+        }
+        else{
+            // 금전
+            imageMoney.setVisibility(View.VISIBLE);
+            image.setVisibility(View.GONE);
+            imageInfo.setVisibility(View.GONE);
+            moneyment.setVisibility(View.VISIBLE);
+            goodsment.setVisibility(View.GONE);
+        }
+
 
     }
 }
